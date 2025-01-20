ac77c608b085754c1b8d85be90bb73471e26f80b
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-01-20 19:27:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
+const url = https://ecommerce-clothes-backend.onrender.com;
+    
 app.use(express.json());
 app.use(cors());
 
@@ -36,7 +38,7 @@ app.use('/images',express.static('upload/images'));
 app.post("/upload",upload.single('product'),(req, res) => {
     res.json({
         success: 1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`${url}:${port}/images/${req.file.filename}`
     })
 });
 
