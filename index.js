b587e6a2f5bfcd5c7885a32184fa51a3819653be
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-02-15 21:30:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // const port = 4000;
-const port =  process.env.PORT || 4000;
+const port =  process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
